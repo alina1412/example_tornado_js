06cb5107c296348e897f6cd0cf5d1e76af432584
alina1412 <alina1412@mail.ru]>
2023-02-01 14:37:54 +0300

    upd alert

--- tornado_js_post/static/js/script3.js
+++ tornado_js_post/static/js/script3.js
@@ -5,14 +5,14 @@ $(document).ready(function () {
         // const data = new FormData(event.target);
         // const message = data.get('message');
         const message = $('#message').val()
-        
+
         console.log("here1");
         console.log("will be val message");
         console.log("here2");
         console.log(message);
         console.log("here3");
-        console.log("here4");
-        
+
+
 
         if (!message) {
             // event.preventDefault();
@@ -20,21 +20,44 @@ $(document).ready(function () {
             return false;
         }
 
-        let body = JSON.stringify({"message": message});
+        let body = JSON.stringify({
+            "message": message
+        });
         $.post("http://localhost:8888/form/", body)
-        .done(function(data) { console.log(data) });
+            .done(function (data) {
+                console.log(data)
+            });
         console.log('posted');
 
+        showAlert();
         return true;
     }
 
-    $('#button1').on('click', (event) => {    
+    $('#button1').on('click', (event) => {
         event.preventDefault();
 
         if (handleSubmit(event)) {
             return true;
-        }    
+        }
         return false;
-      });
-    
+    });
+
 })
+
+
+var returnAlert = async function () {
+    await sleep(1000);
+    $("#success-alert").hide();
+    $("#success-alert").animate({
+        opacity: 0,
+        top: "+=120"
+    }, 1);
+}
+// Показ alerta "Сохранено" с анимацией и возврат returnAlert на место
+var showAlert = async function () {
+    $("#success-alert").show();
+    $("#success-alert").animate({
+        opacity: 1,
+        top: "-=120"
+    }, 2100, returnAlert);
+}
