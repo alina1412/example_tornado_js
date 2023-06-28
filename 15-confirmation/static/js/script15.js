8fb5422b1506a5263572a02c2fd717e636b5de40
alina1412 <alina1412@mail.ru]>
2023-06-28 09:18:46 +0300

    style a little

--- 15-confirmation/static/js/script15.js
+++ 15-confirmation/static/js/script15.js
@@ -4,15 +4,20 @@ let deleteIt = function (event, btn) {
   event.preventDefault();
   let newVal = $("#message").val();
 
-  $('.popover').removeClass('show'); // if other confirmation window  - hide it
+  $('.popover').removeClass('show');      // if other confirmation window  - hide it
   let selected = $(btn).data('selected'); // list of dictionaries
+
+  let html_content = ('accepted:<br/>' + 
+                      selected.map(x => '<span> ' + x.login + ' ' + x.when + '</span>')
+                      .join(',<br/>'));
 
   $(btn).confirmation(
     {
-      rootSelector: '[data-toggle=confirmation]', singleton: true,
+      rootSelector: '[data-toggle=confirmation]', 
+      singleton: true,
       title: 'Delete?',
       html: true,
-      content: 'accepted:<br/>' + selected.map(x => '<span> ' + x.login + ' ' + x.when + '</span>').filter((x,y,z) => z.indexOf(x) == y).join(',<br/>'),
+      content: html_content,
       btnOkLabel: 'Yes',
       btnCancelLabel: 'No',
       customClass: 'popover-success',
@@ -21,11 +26,14 @@ let deleteIt = function (event, btn) {
           url: '/',
           type: 'POST',
           data: { "message": newVal }
-        }).done(function (result) {
-          alert('Success');
-        }).fail(function (result) {
-          alert('error');
-        })
+        }
+        )
+          .done(function (result) {
+            alert('Success');
+          })
+          .fail(function (result) {
+            alert('error');
+          })
       }
     });
   $(btn).confirmation('show');
